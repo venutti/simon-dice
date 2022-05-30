dc69c755dd6ce177801c0c5686f7c69d1d22b74a
venutti <lvenutti@fi.uba.ar>
2022-05-30 19:56:47 -0300

    agrego loop del juego
    La maquina puede dar un color aleatorio para el juego de Simon.
    
    El loop es para evaluar si seguir jugando o no, dadas determinadas condiciones (a hacer!)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log("Está funcionando!");
+const $botonComenzar = document.querySelector("#boton-comenzar");
+
+$botonComenzar.onclick = function(event) {
+
+    ejecutarSimonDice();
+
+    console.log("Terminó el juego!");
+
+    event.preventDefault();
+}
+function obtenerElementoAleatorio(array) {
+    const indiceAleatorio = Math.floor(Math.random() * array.length);
+    return array[indiceAleatorio];
+}
+
+function ejecutarSimonDice() {
+    const colores = ["rojo", "amarillo", "azul", "verde"];
+    const secuenciaSimon = [];
+    let seguirJugando = true;
+
+    while(seguirJugando) {
+        const nuevoColor = obtenerElementoAleatorio(colores);
+        secuenciaSimon.push(nuevoColor);
+
+        // esperar interaccion del usuario
+
+        console.log(secuenciaSimon);
+        if(secuenciaSimon.length > 20) {
+            seguirJugando = false;
+        }
+    }
+}
