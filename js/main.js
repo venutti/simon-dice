ca61ecc314c9a95378f02b04b641eedab223bbc6
venutti <lvenutti@fi.uba.ar>
2022-05-30 22:15:11 -0300

    primera version interactiva
    Puede comenzar un juego y responder a las acciones del usuario.
    
    Eliminado el bucle while, pues las funciones .onclick ya quedan a la espera de eventos.
    
    Usa variables globales porque no se encontró una mejor manera.
    
    Por ahora, todo se muestra por consola.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,71 @@
+let secuenciaSimon = [];
+let cantMovimientosJugador = 0;
+
+const $botonInformacion = document.querySelector("#boton-informacion");
 const $botonComenzar = document.querySelector("#boton-comenzar");
 
 $botonComenzar.onclick = function(event) {
-
     ejecutarSimonDice();
-
-    console.log("Terminó el juego!");
-
     event.preventDefault();
 }
+
 function obtenerElementoAleatorio(array) {
     const indiceAleatorio = Math.floor(Math.random() * array.length);
     return array[indiceAleatorio];
 }
 
 function ejecutarSimonDice() {
+    agregarColorSimon();
+    mostrarSecuenciaSimon();
+    activarListeners();
+}
+
+function reiniciarSimon() {
+    secuenciaSimon = [];
+    cantMovimientosJugador = 0;
+}
+
+function agregarColorSimon() {
     const colores = ["rojo", "amarillo", "azul", "verde"];
-    const secuenciaSimon = [];
-    let seguirJugando = true;
+    const nuevoColor = obtenerElementoAleatorio(colores);
+    secuenciaSimon.push(nuevoColor);
+}
 
-    while(seguirJugando) {
-        const nuevoColor = obtenerElementoAleatorio(colores);
-        secuenciaSimon.push(nuevoColor);
+function mostrarSecuenciaSimon() {
+    console.log(secuenciaSimon);
+}
 
-        // esperar interaccion del usuario
+function activarListeners() {
+    const $botones = document.querySelectorAll("#tablero-simon .color-simon");
+    $botones.forEach(function($boton) {
+        $boton.addEventListener("click", evaluarColorUsuario);
+    })
+}
 
-        console.log(secuenciaSimon);
-        if(secuenciaSimon.length > 20) {
-            seguirJugando = false;
-        }
+function desactivarListeners() {
+    const $botones = document.querySelectorAll("#tablero-simon .color-simon");
+    $botones.forEach(function($boton) {
+        $boton.removeEventListener("click", evaluarColorUsuario);
+    })
+}
+
+function evaluarColorUsuario(event) {
+    const colorUsuario = event.target.id;
+    if(colorUsuario === secuenciaSimon[cantMovimientosJugador]) {
+        cantMovimientosJugador++;
+        evaluarEstadoJuego();
+    } else {
+        console.log("Perdiste HDP!");
+        desactivarListeners();
+        reiniciarSimon();
+    }
+}
+
+function evaluarEstadoJuego() {
+    if(secuenciaSimon.length === cantMovimientosJugador) {
+        agregarColorSimon();
+        cantMovimientosJugador = 0;
+        console.log("Bieen!");
+        mostrarSecuenciaSimon();
     }
 }
